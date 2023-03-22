100eaf7a4812d5b2541200a4d6a6099dc2d4e8a2
MikhailPL <muke.jry@gmail.com>
2023-03-22 21:14:41 +0300

    editing a solution

---
+++ Example/Program.cs
@@ -0,0 +1,58 @@
+﻿// Метод, для приема данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Вводим метод, который будет выводить одномерный массив, содержащий строки
+void Print1DArry(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"[{array[i]}]\t");
+    }
+
+}
+
+// Вводим метод, который находит в массиве строки,
+// длина которых не больше введенного значения,
+// возвращает массив из полученных строк
+string [] GetNewArray(string [] arry, int count)
+{   
+    string [] result = new string [0];
+    for (int i = 0; i < arry.Length; i++)
+    {
+        if (arry[i].Length<=count)
+        {
+            result = result.Concat(new string[] { arry[i] }).ToArray();
+        }
+    }
+    return result;    
+}
+
+string [] Main(int NumLenght)
+{
+    string[] strs = new string[NumLenght]; //Объявляем массив строк длиной n (которую ввёл пользователь)
+
+    for (int i = 0; i < NumLenght; i++)
+        {
+            Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+            strs[i] = Console.ReadLine(); //Заполняем массив
+        }
+        Console.WriteLine("Вы ввели следующие строки:");
+        for (int i = 0; i < NumLenght; i++)
+        {
+            Console.WriteLine(strs[i]);
+        }
+    return strs;
+}
+
+int NumLine = ReadData("Введите колличество строк: ");
+
+string [] arr1D = Main(NumLine);
+
+string [] resultStrarry = GetNewArray(arr1D,3);
+
+Console.WriteLine("Отсортированный массив: \t");
+Print1DArry(resultStrarry);
