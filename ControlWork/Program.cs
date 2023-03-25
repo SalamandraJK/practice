acfc126fbeb9c54a67dc3c5bc59cb1d423cf4b48
MikhailPL <muke.jry@gmail.com>
2023-03-25 20:41:41 +0300

    Вводим метод примема строк для массива и метод для его сортировки

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -17,3 +17,40 @@ void Print1DArry(string[] array)
 
 }
 
+// Вводим метод, который находит в массиве строки,
+// длина которых не больше введенного значения,
+// возвращает массив из полученных строк
+string [] GetNewArray(string [] arry, int count)
+{   
+    string [] result = new string [0];
+    for (int i = 0; i < arry.Length; i++)
+    {
+        if (arry[i].Length<=count)
+        {
+            result = result.Concat(new string[] { arry[i] }).ToArray();
+        }
+    }
+    return result;    
+}
+
+string [] Main(int NumLenght)
+{
+    // Объявляем массив с заданной длинной строк
+    string[] strs = new string[NumLenght];
+
+    for (int i = 0; i < NumLenght; i++)
+        {
+             //Заполняем массив
+            Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+            strs[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Вы ввели следующие строки:");
+
+        for (int i = 0; i < NumLenght; i++)
+        {
+            Console.WriteLine(strs[i]);
+        }
+    return strs;
+}
+
