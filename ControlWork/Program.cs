cac939c450993d6ea1db7aab5a6d113d73b0c871
MikhailPL <muke.jry@gmail.com>
2023-03-25 20:40:29 +0300

    edits file controlwork

---
+++ ControlWork/Program.cs
@@ -0,0 +1,19 @@
+﻿//ControlWork
+
+// Метод, для приема данных
+int ReadData(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Вводим метод, который будет выводить одномерный массив, содержащий строки
+void Print1DArry(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"[{array[i]}]\t");
+    }
+
+}
+
